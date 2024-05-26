ee82f1d94b599e797cbc84a454458242f11351a7
LJoyClifford <ljoyclifford@gmail.com>
2024-05-26 00:31:29 -0400

    Create header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <h1>Welcome to eShop</h1>
+    <>
+    <Header />
+    <main className="py-3">
+      <h1>Welcome to eShop</h1>
+    </main>
+    <Footer />
+    </>
   )
-}
-
-export default App
+};
+export default App;
