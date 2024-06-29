d23c88a39af30ba816c99efeb27efccb7838c43e
LJoyClifford <ljoyclifford@gmail.com>
2024-06-29 18:30:04 -0400

    user email and password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,21 @@ const deleteUser = asyncHandler(async (req, res) => {
 //PUT /api/users/:id
 //Private/admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send('update user');
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 export {
