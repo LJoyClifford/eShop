495efd338e61b2dfab0a5a1d71e46d1c0adaedca
LJoyClifford <ljoyclifford@gmail.com>
2024-07-01 23:20:56 -0400

    backend authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,27 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 //Auth user get token
 //POST /api/users/login
 //Public
 const authUser = asyncHandler(async (req, res) => {
-    res.send('auth user');
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 //register user 
