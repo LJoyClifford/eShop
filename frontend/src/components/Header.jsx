83b393758ab6cb375d025ff7312b61705c3821d4
LJoyClifford <ljoyclifford@gmail.com>
2024-07-05 15:38:34 -0400

    auth and user api slice, login screen, login functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,20 @@
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 
 
 const Header = () => {
 
     const { cartItems } = useSelector((state) => state.cart);
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const logoutHandler = () => {
+        console.log('logout');
+    };
         
   return (
     <header>
@@ -35,9 +41,21 @@ const Header = () => {
                             }
                             </Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to='/login'>
+                        { userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>
+                                    Logout
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (  <LinkContainer to='/login'>
                         <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
                         </LinkContainer>
+
+                        )}
+                       
                     </Nav>
                 </Navbar.Collapse>
             </Container>
