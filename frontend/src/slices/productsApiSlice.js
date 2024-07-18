10ec7833fab4580bd3ce738d28c70d0e11988f10
Lisa Clifford <78510843+LJoyClifford@users.noreply.github.com>
2024-07-17 21:49:50 -0400

    meta

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL, UPLOAD_URL } from '../constants';
+import { PRODUCTS_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -34,7 +34,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
     }),
     uploadProductImage: builder.mutation({
       query: (data) => ({
-        url: `${UPLOAD_URL}`,
+        url: `/api/upload`,
         method: 'POST',
         body: data,
       }),
